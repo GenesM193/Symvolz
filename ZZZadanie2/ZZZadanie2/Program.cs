cb1d7e887271d9d9fc15ce90b7653378cc3e2c78
GenesM193 <danielburashev@gmail.com>
2025-08-03 20:42:38 +0400

    Add files via upload

--- ZZZadanie2/ZZZadanie2/Program.cs
+++ ZZZadanie2/ZZZadanie2/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 Console.WriteLine("Сколько строк?");
-int a = Convert.ToInt32(Console.ReadLine());
+int a;
+string a2 = Console.ReadLine();
+bool a1 = int.TryParse(a2, out a);
 int e = 0;
 Console.WriteLine("Введите информацию из этих строк");
 String line = "";
