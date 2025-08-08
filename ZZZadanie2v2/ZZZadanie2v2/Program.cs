25b1a6890f2788db6caa6fe5f80ff100ad253ac0
GenesM193 <danielburashev@gmail.com>
2025-08-08 19:20:12 +0400

    Add files via upload

--- ZZZadanie2v2/ZZZadanie2v2/Program.cs
+++ ZZZadanie2v2/ZZZadanie2v2/Program.cs
@@ -1,8 +1,10 @@
 ﻿Console.WriteLine("Сколько строк?");
 int a;
+char d;
 string a2 = Console.ReadLine();
 if (!int.TryParse(a2, out a))
     return;
+
 int e = 0;
 Console.WriteLine("Введите информацию из этих строк");
 String line = "";
@@ -12,7 +14,9 @@ for (int i = a; i !=0; i--)
 }
 int b = line.Length - 1;
 Console.WriteLine("Какие символы посчитать?");
-char d = Convert.ToChar(Console.ReadLine());
+string d2 = Console.ReadLine();
+if (!char.TryParse(d2, out d))
+    return;
 for (int j = b; j !=-1; j--)
 {
     char x = line[j];
--- ZZZadanie2v3/ZZZadanie2v3/Program.cs
+++ ZZZadanie2v3/ZZZadanie2v3/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 Console.WriteLine("Сколько строк?");
 int a;
+char d;
 string a2 = Console.ReadLine();
 if (!int.TryParse(a2, out a))
     return;
@@ -14,7 +15,9 @@ while (a != 0)
 }
 int b = line.Length - 1;
 Console.WriteLine("Какие символы посчитать?");
-char d = Convert.ToChar(Console.ReadLine());
+string d2 = Console.ReadLine();
+if (!char.TryParse(d2, out d))
+    return;
 foreach (char z in line)
 {
     if(z == d) e++;
